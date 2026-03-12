30c6835ba42a21b641a1b3f3f6ad2e2f61081aaa
Santiago Vega <santiagovega1478@gmail.com>
2026-03-12 18:12:25 -0300

    feat: add file upload input

--- lib/forth_web_web/live/forth_live.ex
+++ lib/forth_web_web/live/forth_live.ex
@@ -1,29 +1,54 @@
 defmodule ForthWebWeb.ForthLive do
   use ForthWebWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok,
-      assign(socket,
-        program: "",
-        result: "",
-        history: []
-      )}
+  def mount(_, _, socket) do
+    socket =
+      socket
+      |> assign(program: "", result: "")
+      |> allow_upload(:forth_file,
+          accept: :any,
+          max_entries: 1,
+          auto_upload: true,
+          progress: &handle_progress/3
+        )
+
+    {:ok, socket}
+  end
+
+  def handle_progress(:forth_file, entry, socket) do
+    if entry.done? do
+      [program] =
+        consume_uploaded_entries(socket, :forth_file, fn %{path: path}, _ ->
+          {:ok, File.read!(path)}
+        end)
+
+      result =
+        case Forth.eval(program) do
+          {:ok, stack} -> inspect(stack, charlists: :as_lists)
+          {:error, err} -> "Error: #{err}"
+        end
+
+      {:noreply, assign(socket, result: result)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def handle_event("upload", _params, socket) do
+    {:noreply, socket}
   end
 
   def handle_event("run", %{"program" => program}, socket) do
     result =
       case Forth.eval(program) do
-        {:ok, stack} -> inspect(stack)
-        {:error, err} -> "Error: #{err}"
+        {:ok, stack} -> inspect(stack, charlists: :as_lists)
+        {:error, err} ->"Error: #{err}"
       end
 
-    history = [{program, result} | socket.assigns.history]
-
     {:noreply,
-      assign(socket,
-        program: program,
-        result: result,
-        history: history
-      )}
+    assign(socket,
+      program: program,
+      result: result
+    )}
   end
 end
--- test/forth_test.exs
+++ test/forth_test.exs
@@ -11,7 +11,7 @@ defmodule ForthTest do
       assert eval("5") == {:ok, [5]}
     end
 
-    test "parses single negative integer"
+    test "parses single negative integer" do
       assert eval("-5") == {:ok, [-5]}
     end
 
@@ -407,7 +407,6 @@ defmodule ForthTest do
     end
 
     test "handles tabs and mixed whitespace" do
-      # \t es un tabulador, \r es un retorno de carro
       assert eval("1\t2\r\n+") == {:ok, [3]}
     end
 
@@ -425,7 +424,6 @@ defmodule ForthTest do
       5
       double
       """
-
       assert eval(input) == {:ok, [10]}
     end
   end
