9aff366e42af2c901e70331bbab89c974ddfcd81
Santiago Vega <santiagovega1478@gmail.com>
2026-03-14 20:39:49 -0300

    feat: add file error handling

--- lib/forth_web/live/forth_live.ex
+++ lib/forth_web/live/forth_live.ex
@@ -22,36 +22,35 @@ defmodule ForthWeb.ForthLive do
         auto_upload: true,
         progress: &handle_progress/3
       )
-
-    {:ok, socket}
+    {:ok, assign(socket, upload_error: nil)}
   end
 
   def handle_progress(:forth_file, entry, socket) do
     if entry.done? do
-      [program] =
-        consume_uploaded_entries(socket, :forth_file, fn %{path: path}, _ ->
+      try do
+        [program] = consume_uploaded_entries(socket, :forth_file, fn %{path: path}, _ ->
           {:ok, File.read!(path)}
         end)
 
-      result = eval_program(program)
+        result = eval_program(program)
+        filename = entry.client_name
+        eval = save_evaluation(program, result, filename)
 
-      filename = entry.client_name
-
-      eval = save_evaluation(program, result, filename)
-
-      socket =
+        {:noreply,
         socket
-        |> assign(result: result)
-        |> Phoenix.Component.update(:history, fn h -> [eval | h] end)
-
-      {:noreply, socket}
+        |> assign(result: result, upload_error: nil)
+        |> Phoenix.Component.update(:history, fn h -> [eval | h] end)}
+      rescue
+        _ ->
+          {:noreply, assign(socket, upload_error: "Invalid file encoding or binary data.")}
+      end
     else
       {:noreply, socket}
     end
   end
 
   def handle_event("upload", _params, socket) do
-    {:noreply, socket}
+    {:noreply, assign(socket, upload_error: nil)}
   end
 
   def handle_event("run", %{"program" => program}, socket) do
@@ -69,11 +68,11 @@ defmodule ForthWeb.ForthLive do
   end
 
   defp eval_program(program) do
-    case Forth.eval(program) do
-      {:ok, stack} -> inspect(stack, charlists: :as_lists)
-      {:error, err} -> "Error: #{err}"
+        case Forth.eval(program) do
+          {:ok, stack} -> inspect(stack, charlists: :as_lists)
+          {:error, err} -> "Error: #{err}"
+        end
     end
-  end
 
   defp save_evaluation(program, result, source) do
     %Evaluation{}
